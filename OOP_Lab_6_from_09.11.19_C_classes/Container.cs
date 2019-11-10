626601bb3c5e19844adcf674ea212989e9d8fabc
Zeratul352 <43727651+Zeratul352@users.noreply.github.com>
2019-11-10 11:50:01 +0200

    Final Version
    It works! I have no idea why)

--- OOP_Lab_6_from_09.11.19_C_classes/Container.cs
+++ OOP_Lab_6_from_09.11.19_C_classes/Container.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,25 +7,36 @@ using System.Threading.Tasks;
 
 namespace OOP_Lab_6_from_09._11._19_C_classes
 {
-    class EmailContainer : IEnumerator // вот так вот нельзя
+    class EmailContainer : IEnumerable // mail container
     {
         List <Email> emails;
-        //List <Telephone> telephones;
+      
         
+
+        public IEnumerator GetEnumerator()
+        {
+           return emails.GetEnumerator();
+        }
+
        public EmailContainer()
         {
             emails = new List<Email>();
-            //telephones = new List<Telephone>();
+            
         }
        public EmailContainer(List<Email> mail) 
         {
             emails = new List<Email>();
-            //telephones = new List<Telephone>();
+            
             emails = mail;
-            //telephones = phone;
+            
         }
-       public void AddEmail(Email mail)
-        { emails.Add(mail); }
+       public void AddEmail()
+        {
+            string mail = Console.ReadLine();
+            string dom = Console.ReadLine();
+            Email A = new Email(mail, dom);
+            emails.Add(A);
+        }
        
        public void RemoveEmail(int index)
         { emails.Remove(emails[index]); }
@@ -45,20 +57,61 @@ namespace OOP_Lab_6_from_09._11._19_C_classes
                 
                 }
             }
-            /*for(int i = 0; i < telephones.Count; i++)
+            
+        }
+
+    }
+
+    class TelephoneContainer : IEnumerable
+    {
+        
+
+        List <Telephone> telephones;
+
+        public IEnumerator GetEnumerator()
+        {
+            return telephones.GetEnumerator();
+        }
+
+        public TelephoneContainer()
+        {
+            telephones = new List<Telephone>();
+            
+        }
+        public TelephoneContainer(List<Telephone> phones)
+        {
+            telephones = new List<Telephone>();
+            
+            telephones = phones;
+            
+        }
+        public void AddTelephone()
+        {
+            string code = Console.ReadLine();
+            string num = Console.ReadLine();
+            string strana = Console.ReadLine();
+            Telephone A = new Telephone(num, code, strana);
+            telephones.Add(A);
+        }
+
+        public void RemoveEmail(int index)
+        { telephones.Remove(telephones[index]); }
+        public void Sort()
+        {
+            Telephone temptelephone = new Telephone();
+            for(int i = 0; i < telephones.Count; i++)
             {
                 for(int j = 0; j < telephones.Count - i - 1; j++)
                 {
-                    if(string.Compare(telephones[j].toString(), telephones[j + 1].toString()) > 0)
+                    if(string.Compare(telephones[j].ToString(), telephones[j + 1].ToString()) > 0)
                     {
                         temptelephone = telephones[j];
                         telephones[j] = telephones[j + 1];
                         telephones[j + 1] = temptelephone;
                     }
                 }
-            }*/
+            }
         }
 
     }
-   
 }
--- OOP_Lab_6_from_09.11.19_C_classes/Program.cs
+++ OOP_Lab_6_from_09.11.19_C_classes/Program.cs
@@ -10,6 +10,27 @@ namespace OOP_Lab_6_from_09._11._19_C_classes
     {
         static void Main(string[] args)
         {
+            EmailContainer Mailbox = new EmailContainer();
+            for(int i = 0; i < 3; i++)// ввод, сортировка по возрастанию и вывод 3х мыл
+            {
+                Mailbox.AddEmail();
+            }
+            Mailbox.Sort();
+            foreach(object item in Mailbox)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            TelephoneContainer NumberBook = new TelephoneContainer();
+            for(int i = 0; i < 2; i++)// ввод, сортировка по возрастанию и вывод 2х телефонов
+            {
+                NumberBook.AddTelephone();
+            }
+            NumberBook.Sort();
+            foreach(object item in NumberBook)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            
         }
     }
 }
